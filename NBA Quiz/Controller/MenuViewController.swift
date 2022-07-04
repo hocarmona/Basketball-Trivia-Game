4ecebc5e8a954f28ca13e206fe9c63e4c9d39c7f
Orlando Carmona <orcabe86@gmail.com>
2022-07-04 00:07:24 -0500

    MenuViewController UI Finished

---
+++ NBA Quiz/Controller/MenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBA Quiz
+//
+//  Created by Hector Carmona on 7/3/22.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playButton.layer.cornerRadius = 25
+    }
+
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
---
+++ NBA Quiz/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  NBA Quiz
+//
+//  Created by Hector Carmona on 7/3/22.
+//
+
+import Foundation
