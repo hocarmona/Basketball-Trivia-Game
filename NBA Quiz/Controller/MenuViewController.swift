f10942363bc54cbdd6f0ae5e83ae59cf5d260d39
Orlando Carmona <orcabe86@gmail.com>
2022-07-11 12:32:20 -0500

    Conffeti added to menu screen and result screen.

--- NBA Quiz/Controller/MenuViewController.swift
+++ NBA Quiz/Controller/MenuViewController.swift
@@ -6,20 +6,30 @@
 //
 
 import UIKit
+import SwiftConfettiView
 
 class MenuViewController: UIViewController {
 
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var backgroundView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.setHidesBackButton(true, animated: false)
         playButton.layer.cornerRadius = 25
+        conffetiAnimation()
     }
 
     @IBAction func playButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToQuestions", sender: self)
         
+    }
+    
+    func conffetiAnimation() {
+        let conffetiView = SwiftConfettiView(frame: self.view.bounds)
+        conffetiView.type = .image(UIImage(named: "bbback")!)
+        backgroundView.addSubview(conffetiView)
+        conffetiView.startConfetti()
     }
     
 }
--- NBA Quiz/Controller/ResultViewController.swift
+++ NBA Quiz/Controller/ResultViewController.swift
@@ -6,25 +6,41 @@
 //
 
 import UIKit
+import SwiftConfettiView
 
 class ResultViewController: UIViewController {
 
     @IBOutlet weak var scoreNumberLabel: UILabel!
     @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var backgroundView: UIImageView!
+    
     
     var correctAnswers: Int = 0
     var totalOfQuestions: Int = 0
-    
-    
+    var confettiView = SwiftConfettiView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\(correctAnswers) / \(totalOfQuestions)")
         navigationItem.setHidesBackButton(true, animated: false)
         menuButton.layer.cornerRadius = 15
         scoreNumberLabel.text = "\(correctAnswers)/\(totalOfQuestions)"
-        
+        conffetiAnimation()
+
     }
     
-
+    @IBAction func menuButtonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: K.goToMenu, sender: self)
+    }
+    
+    func conffetiAnimation() {
+        let conffetiView = SwiftConfettiView(frame: self.view.bounds)
+        conffetiView.type = .confetti
+        backgroundView.addSubview(conffetiView)
+        conffetiView.intensity = 0.90
+        conffetiView.startConfetti()
+    }
+    
     
 }
